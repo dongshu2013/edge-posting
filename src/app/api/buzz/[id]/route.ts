89b176f826ec001f2d1b90f8934c594a688f67c3
dongshu2013 <dongs2011@gmail.com>
2025-03-08 11:16:23 -0800

    add back sort and show active buzz for my buzz page

--- src/app/api/buzz/[id]/route.ts
+++ src/app/api/buzz/[id]/route.ts
@@ -3,9 +3,9 @@ import { prisma } from '@/lib/prisma';
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: { id: string } }
 ) {
-  const id = (await params).id;
+  const { id } = params;
   
   if (!id) {
     return NextResponse.json(

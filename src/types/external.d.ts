e307843c40bfecb877eed224e088c00c0226de4f
dongshu2013 <dongs2011@gmail.com>
2025-03-21 18:57:09 -0700

    add google kms util functions

---
+++ src/types/external.d.ts
@@ -0,0 +1,18 @@
+declare module 'asn1.js' {
+  import BN from 'bn.js';
+  
+  interface ASN1Options {
+    seq(): ASN1Options;
+    obj(...args: any[]): void;
+    key(name: string): ASN1Options;
+    int(): void;
+  }
+
+  export function define(name: string, fn: (this: ASN1Options) => void): {
+    decode(buffer: Buffer, format: string): { r: BN; s: BN };
+  };
+}
+
+declare module 'fast-crc32c' {
+  export function calculate(input: string | Buffer): number;
+}

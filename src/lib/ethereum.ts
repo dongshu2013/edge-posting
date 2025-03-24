a94573444d72a50b303b0dd7c590d4f830b7f082
vzxh <wkq7740@gmail.com>
2025-03-24 09:52:56 +0800

    refine wagmi config

--- src/lib/ethereum.ts
+++ src/lib/ethereum.ts
@@ -1,35 +1,27 @@
 import {
   Chain,
   createPublicClient,
-  createWalletClient,
-  erc20Abi,
   http,
-  PublicClient,
+  PublicClient
 } from "viem";
-import { privateKeyToAccount } from "viem/accounts";
-import { base, baseSepolia, bscTestnet } from "viem/chains";
+import { base, baseSepolia, bsc, bscTestnet } from "viem/chains";
 
 export const ethereumConfigs: {
   [key: number]: {
     chain: Chain;
-    tokenAddress: `0x${string}`;
-    tokenDecimals: number;
   };
 } = {
   84532: {
     chain: baseSepolia,
-    tokenAddress: "0x3869E143da147C391258950978B16266e1711a5D",
-    tokenDecimals: 6,
   },
   8453: {
     chain: base,
-    tokenAddress: "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2",
-    tokenDecimals: 6,
   },
   97: {
     chain: bscTestnet,
-    tokenAddress: "0x3869E143da147C391258950978B16266e1711a5D",
-    tokenDecimals: 6,
+  },
+  56: {
+    chain: bsc,
   },
 };
 

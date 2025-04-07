36a7fa2841a846680ac2c3541c7df30b34f7cb3f
vzxh <wkq7740@gmail.com>
2025-04-07 11:30:21 +0800

    fix deadline

--- src/app/buzz/new/page.tsx
+++ src/app/buzz/new/page.tsx
@@ -232,8 +232,8 @@ export default function NewBuzzPage() {
     }
 
     const deadline = new Date();
-    // deadline.setHours(deadline.getHours() + Number(formData.deadline));
-    deadline.setMinutes(deadline.getMinutes() + 5);
+    deadline.setHours(deadline.getHours() + Number(formData.deadline));
+    // deadline.setMinutes(deadline.getMinutes() + 5);
 
     try {
       setIsCreatingBuzz(true);
--- src/app/kol/page.tsx
+++ src/app/kol/page.tsx
@@ -52,7 +52,7 @@ export default function KolPage() {
         </button>
       </div>
     ) : userKolStatus === "pending" ? (
-      <div className="mt-6">
+      <div className="hidden mt-6">
         <button
           disabled
           className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
